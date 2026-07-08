904402ec791cbffb5578d50ab10fa079067d69f9
Sachin <141260725+sachin7444@users.noreply.github.com>
2026-07-08 21:48:15 +0530

    update

--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -1,10 +1,26 @@
 import prisma from '../lib/prisma.js';
 
 export const getPosts = async (req, res) => {
+    const query = req.query;
     try{
-        const posts = await prisma.post.findMany();
+        const posts = await prisma.post.findMany({
+            where:{
+                city: query.city || undefined,
+                type: query.type || undefined,
+                property: query.property || undefined,
+                bedroom: query.bedroom ? parseInt(query.bedroom) : undefined,
+                bathroom: query.bathroom ? parseInt(query.bathroom) : undefined,
+                price:{
+                    gte: query.minPrice || 0,
+                    lte: query.maxPrice || 100000,
+                    
+                }
+            }
+        });
 
-        res.status(200).json(posts);
+        setTimeout(() => {
+            res.status(200).json(posts);
+        },3000);
     }catch(err){
         console.log(err);
         res.status(500).json({message:"Failed to get posts"})
@@ -38,19 +54,21 @@ export const addPost = async (req, res) => {
     const body = req.body;
     const tokenUserId = req.userId;
     try{
+        const {postDetails, ...postData} = body;
+
         const newPost = await prisma.post.create({
             data:{
-                ...body.postData,
+                ...postData,
                 userId: tokenUserId,
                 postDetails:{
-                    create:body.postDetails,
+                    create:postDetails,
                 },
             }
         })
         res.status(200).json(newPost);
     }catch(err){
         console.log(err);
-        res.status(500).json({message:"Failed to adds posts"})
+        res.status(500).json({message:"Failed to add post"})
     }
 }
 export const updatePost = async (req, res) => {
